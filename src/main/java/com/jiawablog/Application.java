5ccd2ca8079a76fa3130981bdce151c152054649
jiawahome <82144921@qq.com>
2019-03-29 23:41:06 +0800

    修改端口号和启动日志

--- src/main/java/com/jiawablog/Application.java
+++ src/main/java/com/jiawablog/Application.java
@@ -1,11 +1,23 @@
 package com.jiawablog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Application {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("根地址: \t\thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 }
