35644003687cd2e7d10e29eed3f44d4a1615d8d7
jiawahome <82144921@qq.com>
2019-04-02 22:04:31 +0800

    增加数据库脚本

---
+++ doc/数据库/all.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` CHAR(8) NOT NULL DEFAULT '' COMMENT 'ID',
+  `login_name` VARCHAR(50) NOT NULL COMMENT '登陆名',
+  `password` VARCHAR(50) NOT NULL COMMENT '密码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
