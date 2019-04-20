a84a3010394465094e8383897747008b044ef36d
jiawahome <82144921@qq.com>
2019-04-20 16:45:49 +0800

    用户列表页面关联数据库记录

--- src/main/java/com/jiawablog/controller/AdminUserController.java
+++ src/main/java/com/jiawablog/controller/AdminUserController.java
@@ -1,10 +1,19 @@
 package com.jiawablog.controller;
 
+import com.jiawablog.db.User;
+import com.jiawablog.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 public class AdminUserController {
+
+    @Resource
+    private UserService userService;
 
     @GetMapping("/admin/user")
     private String user() {
@@ -12,7 +21,9 @@ public class AdminUserController {
     }
 
     @GetMapping("/admin/user/list")
-    private String list() {
+    private String list(Model model) {
+        List<User> userList = userService.list();
+        model.addAttribute("list", userList);
         return "admin/user/list";
     }
 }
